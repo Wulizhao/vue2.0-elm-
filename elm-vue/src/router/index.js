f545f156b187c7a52f0e2ac6b425a374306bd2b0
WuliZhao <291723856@qq.com>
2017-04-25 17:46:40 +0800

    添加路由重定向

--- elm-vue/src/router/index.js
+++ elm-vue/src/router/index.js
@@ -10,6 +10,10 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/goods'
+    },
+    {
+      path: '/',
       component: SellerIndex,
       children: [
         {
